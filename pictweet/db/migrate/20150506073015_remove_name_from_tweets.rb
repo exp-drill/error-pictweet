6810c911bc5a52da1d0b8ec68648229629fe1867
mkoma414 <m-koma@nifty.com>
2018-11-07 14:29:19 +0900

    modify migration files

--- pictweet/db/migrate/20150506073015_remove_name_from_tweets.rb
+++ pictweet/db/migrate/20150506073015_remove_name_from_tweets.rb
@@ -1,4 +1,4 @@
-class RemoveNameFromTweets < ActiveRecord::Migration
+class RemoveNameFromTweets < ActiveRecord::Migration[5.2]
   def change
     remove_column :tweets, :name, :string
   end
--- pictweet/db/schema.rb
+++ pictweet/db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,33 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150506073015) do
+ActiveRecord::Schema.define(version: 2015_05_06_073015) do
 
-  create_table "tweets", force: :cascade do |t|
-    t.text     "text",       limit: 65535
-    t.text     "image",      limit: 65535
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "user_id",    limit: 4
+  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text"
+    t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "nickname",               limit: 255
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "nickname"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
